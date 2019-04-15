5aaebb3f322a922e577d63e2d2b85d9152a9b271
Regnor <serg0g0150800@gmail.com>
2019-04-15 14:43:30 +0400

    15.04/2019

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-var randomMovieArray =['Blade Runner', 'Blade Runner 2049','Blade Runner: Black Out 2022', 'On the Edge of Blade Runner','Dangerous Days: Making Blade Runner', 'Oscar Pistorius: Blade Runner Killer'];
+function Zapros() {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open('GET', `http://www.omdbapi.com/?t=blade+runner&apikey=da2ea953`, false);
+        req.send(null);
+        if (req.status != 200) () => reject();
+        let result = JSON.parse(req.responseText).Search;
+        console.log(req);
+        console.log(req.responseText);
+        console.log((req.responseText).search);
+        console.log(result);
+        resolve(result);
+    });
+}
 
-function apiCall(){
-    jQuery.getJSON('http://www.omdbapi.com/?t=blade+runner+2049'.then(function(response){
-        var image=response.Poster;
-        console.log(image);
-    }));
+function Out(res) {
+    res.forEach(element => {
+        let div = document.createElement('div');
+        div.innerHTML = `<img src=${element.Poster}`;
+        movies.appendChild(div);
+    });
 };
-apiCall();
