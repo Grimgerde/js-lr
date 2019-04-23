078d440357052fea07fcc98a7141caa9eb1d04d2
Grimgerde <ehot2000ehot@yandex.ru>
2019-04-23 11:25:07 +0400

    final version
    final version

--- src/main.js
+++ src/main.js
@@ -1,22 +1,28 @@
 function Zapros() {
-    return new Promise((resolve, reject) => {
-        const req = new XMLHttpRequest();
-        req.open('GET', `http://www.omdbapi.com/?t=blade+runner&apikey=da2ea953`, false);
-        req.send(null);
-        if (req.status != 200) () => reject();
-        let result = JSON.parse(req.responseText).Search;
-        console.log(req);
-        console.log(req.responseText);
-        console.log((req.responseText).search);
-        console.log(result);
-        resolve(result);
-    });
-}
-
-function Out(res) {
-    res.forEach(element => {
-        let div = document.createElement('div');
-        div.innerHTML = `<img src=${element.Poster}`;
-        movies.appendChild(div);
-    });
-};
+    
+    let name="blade runner" 
+    return new Promise((resolve, reject) => { 
+    const req = new XMLHttpRequest(); 
+    req.open('GET', `http://www.omdbapi.com/?s=${name}&apikey=f7a37081`, false); 
+    req.send(null); 
+    if (req.status != 200) () => reject(() => console.log("Ошибка")); 
+    let result = JSON.parse(req.responseText).Search; 
+    resolve(result); 
+    }); 
+    } 
+    function Out(res) { 
+    let movies = document.getElementById("movies"); 
+    res.forEach(element => { //Крутим полученный от сервера массив с объектами фильмов. 
+    let div = document.createElement('div'); 
+    
+    div.innerHTML = `<img src=${element.Poster}>` 
+    movies.appendChild(div); 
+    }); 
+    } 
+    function Start() { 
+    Zapros() 
+    .then(result => Out(result)) 
+    .catch(e => console.log(e)); 
+    } 
+    
+    Start();
