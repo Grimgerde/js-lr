384fc7b65e96b610fe801345940e1c0e498938ae
Grimgerde <EHOT2000EHOT@yandex.ru>
2019-04-23 14:19:38 +0400

    23.04.2019 final

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
 function Zapros() {
-    
-    let name="blade runner" 
-    return new Promise((resolve, reject) => { 
-    const req = new XMLHttpRequest(); 
-    req.open('GET', `http://www.omdbapi.com/?s=${name}&apikey=f7a37081`, false); 
-    req.send(null); 
-    if (req.status != 200) () => reject(() => console.log("Ошибка")); 
-    let result = JSON.parse(req.responseText).Search; 
-    resolve(result); 
-    }); 
-    } 
-    function Out(res) { 
-    let movies = document.getElementById("movies"); 
-    res.forEach(element => { //Крутим полученный от сервера массив с объектами фильмов. 
-    let div = document.createElement('div'); 
-    
-    div.innerHTML = `<img src=${element.Poster}>` 
-    movies.appendChild(div); 
-    }); 
-    } 
-    function Start() { 
-    Zapros() 
-    .then(result => Out(result)) 
-    .catch(e => console.log(e)); 
-    } 
-    
-    Start();
+
+  let name = "blade runner"
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', `http://www.omdbapi.com/?s=${name}&apikey=f7a37081`, false);
+    req.send(null);
+    if (req.status != 200)() => reject(() => console.log("Ошибка"));
+    let result = JSON.parse(req.responseText).Search;
+    resolve(result);
+  });
+}
+
+function Out(res) {
+  let movies = document.getElementById("movies");
+  res.forEach(element => {
+    let img = document.createElement('img');
+
+    img.src = element.Poster;
+    movies.appendChild(img);
+  });
+}
+
+function Start() {
+  Zapros()
+    .then(Out)
+    .catch(e => console.log(e));
+}
+
+Start();
